f0f4352e00fd9f5b37d94ff98071d1e1337d1bed
harika56 <60980049+harika56@users.noreply.github.com>
2020-02-12 16:21:44 -0700

    Add files via upload

---
+++ ExpoPlanet/ExpoPlanet/ExoPlanetData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExoPlanet
+{
+    public class ExoPlanetData
+    {
+        public string PlanetIdentifier { get; set; }
+        public int TypeFlag { get; set; }
+        public object PlanetaryMassJpt { get; set; }
+        public object RadiusJpt { get; set; }
+        public object PeriodDays { get; set; }
+        public object SemiMajorAxisAU { get; set; }
+        public object Eccentricity { get; set; }
+        public object PeriastronDeg { get; set; }
+        public object LongitudeDeg { get; set; }
+        public object AscendingNodeDeg { get; set; }
+        public object InclinationDeg { get; set; }
+        public object SurfaceTempK { get; set; }
+        public object AgeGyr { get; set; }
+        public string DiscoveryMethod { get; set; }
+        public object DiscoveryYear { get; set; }
+        public string LastUpdated { get; set; }
+        public string RightAscension { get; set; }
+        public string Declination { get; set; }
+        public object DistfromSunParsec { get; set; }
+        public object HostStarMassSlrMass { get; set; }
+        public object HostStarRadiusSlrRad { get; set; }
+        public object HostStarMetallicity { get; set; }
+        public object HostStarTempK { get; set; }
+        public object HostStarAgeGyr { get; set; }
+    }
+}
---
+++ ExpoPlanet/ExpoPlanet/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExoPlanet
+{
+   public class Program
+    {
+        static void Main()
+        {
+
+        var exoPlanetsJson = File.ReadAllText("exoplanets.json");
+        var exoPlanets = JsonConvert.DeserializeObject<List<ExoPlanetData>>(exoPlanetsJson);
+        Console.WriteLine($"Number of Planets: {exoPlanets.Count}");
+        Console.WriteLine($"Number of Orphan Planets (without star): {exoPlanets.FindAll( n => n.HostStarAgeGyr.ToString() == "" && n.HostStarRadiusSlrRad.ToString() == " " && n.HostStarMetallicity.ToString() == "" && n.HostStarMassSlrMass.ToString() == "" && n.HostStarTempK.ToString() == "" ).Count}");
+
+        var planetOrbitingHottestStar = exoPlanets.FirstOrDefault(v => v.HostStarTempK.ToString() == exoPlanets.Max(c => c.HostStarTempK.ToString()));
+
+        if (planetOrbitingHottestStar != null)
+                        {
+                            Console.WriteLine($"Planet Orbiting HottestStar: { planetOrbitingHottestStar.PlanetIdentifier}");
+                        }
+          var years = exoPlanets.GroupBy(u => u.DiscoveryYear.OrderBy(s => s.Key.ToString() == "" ? 0 : Convert.ToInt32(s.Key)).Select(g => g.ToList()).ToList();
+
+            foreach (var year in years)
+            {
+                var yea = year.FirstOrDefault()?.DiscoveryYear;
+                var small = year.FindAll(c => c != null && c.RadiusJpt.ToString() != "" && Convert.ToDouble(c.RadiusJpt) < 1);
+                var medium = year.FindAll(c => c! = null && c.RadiusJpt.ToString() != "" && Convert.ToDouble(c.RadiusJpt) < 2 && Convert.ToDouble(c.RadiusJpt) > 1);
+                var large = year.FindAll(c => c != null && c.RadiusJpt.ToString() != "" && Convert.ToDouble(c.RadiusJpt) > 2);
+                Console.WriteLine($"In the year{yea} we discovered {small.Count} Small Planets, {medium.Count} Medium Planets and {large.Count} Large Planets. Total {small.Count + medium.Count + large.Count}");
+            }
+        }
+    }
+}
+   
+
+
